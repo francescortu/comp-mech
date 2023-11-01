e114aaf1bf01a4808f1b0a8daf81db5264420c40
Francesc0rtu <francescortu@live.it>
2023-11-01 10:36:30 +0100

    update

--- src/ablate_heads.py
+++ src/ablate_heads.py
@@ -267,7 +267,7 @@ class OVCircuit:
         copy_score = torch.tensor(list(copy_score.values())).mean().item()
         return copy_score
     
-    def ov_single_len_all_heads_score(self, resid_layer_input, resid_pos:str, length, target="copy", disable_tqdm=False, plot=False):
+    def ov_single_len_all_heads_score(self, resid_layer_input, resid_pos:str, length, target="copy", disable_tqdm=False, plot=False, logit_score=False, resid_read="resid_post"):
         assert target in ["copy", "mem"], "target should be one of copy or mem"
         assert resid_pos in ["o_pre","last_pre", "1_1_subject", "1_2_subject", "1_3_subject", "definition", "2_1_subject", "2_2_subject", "2_3_subject"], "resid_pos should be one of 1_1_subject, 1_2_subject, 1_3_subject, definition, 2_1_subject, 2_2_subject, 2_3_subject"
         self.dataset.set_len(length, self.model)
@@ -298,61 +298,123 @@ class OVCircuit:
             position = self.dataset.obj_pos[0] - 1
         else:
             raise ValueError("resid_pos not recognized: should be one of 1_1_subject, 1_2_subject, 1_3_subject, definition, 2_1_subject, 2_2_subject, 2_3_subject")
-        topk = 10
-        count_target = torch.zeros(( self.model.cfg.n_layers, self.model.cfg.n_heads,  num_batches))
-        for idx, batch in tqdm(enumerate(dataloader), total=num_batches, desc=f"OV circuit at all heads", disable=disable_tqdm):
-            _, cache = self.model.run_with_cache(batch["corrupted_prompts"])
-            for layer in range(self.model.cfg.n_layers):
-                for head in range(self.model.cfg.n_heads):
-                    residual_stream = cache["resid_post", resid_layer_input]
-                    W_OV = (self.model.blocks[layer].attn.W_V @ self.model.blocks[layer].attn.W_O)[head]
-                    logit_output = einops.einsum(self.model.W_U, (residual_stream[:,position,:] @ W_OV), "d d_v, b d -> b d_v")
-                    logit_output = self.model.ln_final(logit_output)
-                    topk_tokens = torch.topk(logit_output, k=topk, dim=-1).indices
-                    target_list = [self.model.to_string(batch["target"][i,target_idx]) for i in range(self.batch_size)]
-                    
-                    count = 0
-                    for i in range(self.batch_size):
-                        if target_list[i] in [self.model.to_string(topk_tokens[i,j]) for j in range(topk)]:
-                            count += 1
-                    count_target[layer, head, idx] = count
-                    
+        if logit_score == False:
+            topk = 10
+            count_target = torch.zeros(( self.model.cfg.n_layers, self.model.cfg.n_heads,  num_batches))
+            for idx, batch in tqdm(enumerate(dataloader), total=num_batches, desc=f"OV circuit at all heads", disable=disable_tqdm):
+                _, cache = self.model.run_with_cache(batch["corrupted_prompts"])
+                for layer in range(self.model.cfg.n_layers):
+                    for head in range(self.model.cfg.n_heads):
+                        residual_stream = cache[resid_read, resid_layer_input]
+                        W_OV = (self.model.blocks[layer].attn.W_V @ self.model.blocks[layer].attn.W_O)[head]
+                        logit_output = einops.einsum(self.model.W_U, (residual_stream[:,position,:] @ W_OV), "d d_v, b d -> b d_v")
+                        logit_output = self.model.ln_final(logit_output)
+                        topk_tokens = torch.topk(logit_output, k=topk, dim=-1).indices
+                        target_list = [self.model.to_string(batch["target"][i,target_idx]) for i in range(self.batch_size)]
+                        
+                        count = 0
+                        for i in range(self.batch_size):
+                            if target_list[i] in [self.model.to_string(topk_tokens[i,j]) for j in range(topk)]:
+                                count += 1
+                        count_target[layer, head, idx] = count
+                        
 
-        total_num_examples = self.batch_size * num_batches
-        count_target = einops.reduce(count_target, "l h b -> l h", reduction="sum")
-        # percentage
-        count_target = 100 * count_target / total_num_examples
-        if plot:
-            self.plot_heatmap(count_target)
-        return count_target       
+            total_num_examples = self.batch_size * num_batches
+            count_target = einops.reduce(count_target, "l h b -> l h", reduction="sum")
+            # percentage
+            count_target = 100 * count_target / total_num_examples
+            if plot:
+                self.plot_heatmap(count_target)
+            return count_target       
+        
+        if logit_score == True:
+            logit_target = torch.zeros(( self.model.cfg.n_layers, self.model.cfg.n_heads,  num_batches, self.batch_size))
+            for idx, batch in tqdm(enumerate(dataloader), total=num_batches, desc=f"OV circuit at all heads", disable=disable_tqdm):
+                _, cache = self.model.run_with_cache(batch["corrupted_prompts"])
+                for layer in range(self.model.cfg.n_layers):
+                    for head in range(self.model.cfg.n_heads):
+                        residual_stream = cache[resid_read, resid_layer_input]
+                        W_OV = (self.model.blocks[layer].attn.W_V @ self.model.blocks[layer].attn.W_O)[head]
+                        logit_output = einops.einsum(self.model.W_U, (residual_stream[:,position,:] @ W_OV), "d d_v, b d -> b d_v")
+                        logit_output = self.model.ln_final(logit_output)
+                        mem_logit, cp_logit = to_logit_token(logit_output, batch["target"])
+                        if target == "copy":
+                            logit_target[layer, head, idx] = cp_logit.cpu()
+                        elif target == "mem":
+                            logit_target[layer, head, idx] = mem_logit.cpu()
+            logit_target = einops.rearrange(logit_target, "l h b s -> l h (b s)")
+            # compute avg logit_target for each layer accross al the position and examples
+            avg_mean = logit_target.mean(dim=-1).mean(dim=-1)
+            #mean over all examples
+            logit_target_std = logit_target.std(dim=-1)
+            logit_target = logit_target.mean(dim=-1)
+            # for each layer, compute the percentage increase or decrease of logit_target
+            for layer in range(self.model.cfg.n_layers):
+                logit_target[layer] = -100 * (logit_target[layer] - avg_mean[layer]) / avg_mean[layer]
+
+            if plot:
+                self.plot_heatmap(logit_target)
+                self.plot_heatmap(logit_target_std)
+            return logit_target
+                        
     
-    def plot_heatmap(self, copy_score, xlabel="head", ylabel="layer", x_ticks=None, y_ticks=None):
+    def plot_heatmap(self, copy_score, xlabel="head", ylabel="layer", x_ticks=None, y_ticks=None, title="none"):
         import seaborn as sns
         import matplotlib.pyplot as plt
         sns.set()
         sns.set_style("whitegrid", {"axes.grid": False})
         fig, ax = plt.subplots(figsize=(13, 8))
-        ax.set_title(f"Copy score for all heads")
-        sns.heatmap(copy_score, annot=True, ax=ax, cmap="YlGnBu", fmt=".1f")
+        ax.set_title(title)
+        sns.heatmap(copy_score, annot=True, ax=ax, cmap="RdBu_r", fmt=".1f", center=0)
         if x_ticks:
             ax.set_xticklabels(x_ticks)
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
+        plt.show()
             
-    def compute_copy_score_all_heads(self, resid_layer_input, resid_pos, target="copy", plot=False):
+    def compute_copy_score_all_heads(self, resid_layer_input, resid_pos, target="copy", plot=False, logit_score=False, **kwargs):
         lenghts = self.dataset.get_lengths()
         copy_score = {} # key: length, value: copy score (layer, head)
         for l in lenghts:
-            copy_score[l] = self.ov_single_len_all_heads_score(resid_layer_input, resid_pos, l, target=target, plot=False, disable_tqdm=False)
+            copy_score[l] = self.ov_single_len_all_heads_score(resid_layer_input, resid_pos, l, target=target, plot=False, disable_tqdm=False, logit_score=logit_score, **kwargs)
         
         # copy_score is a dict of tensors of shape (n_layers, n_heads). Convert to a tensor of shape (n_layers, n_heads) with the mean of the copy score
         copy_score = torch.stack(list(copy_score.values()), dim=0).mean(dim=0)
         
         if plot:
+            # put to zero the negative values
+            copy_score[copy_score < 0] = 0
             self.plot_heatmap(copy_score)
         else:
             return copy_score
     
+    def aggregate_result(self, object_positions, logit_target, length):
+        subject_1_1 = 5
+        subject_1_2 = 6
+        subject_1_3 = 7 if length > 17 else 6
+        subject_2_1 = object_positions + 2
+        subject_2_2 = object_positions + 3
+        subject_2_3 = object_positions + 4 if length > 17 else object_positions + 3
+        last_position = length - 1
+        object_positions_pre = object_positions - 1
+        object_positions_next = object_positions + 1
+        
+        result_aggregate = torch.zeros((self.model.cfg.n_layers, 14))
+        result_aggregate[:,0] = logit_target[:,:subject_1_1].mean(dim=1)
+        result_aggregate[:,1] = logit_target[:,subject_1_1]
+        result_aggregate[:,2] = logit_target[:,subject_1_2]
+        result_aggregate[:,3] = logit_target[:,subject_1_3]
+        result_aggregate[:,4] = logit_target[:,subject_1_3+1:object_positions_pre].mean(dim=1)
+        result_aggregate[:,5] = logit_target[:,object_positions_pre]
+        result_aggregate[:,6] = logit_target[:,object_positions]
+        result_aggregate[:,7] = logit_target[:,object_positions_next]
+        result_aggregate[:,8] = logit_target[:,subject_2_1]
+        result_aggregate[:,9] = logit_target[:,subject_2_2]
+        result_aggregate[:,10] = logit_target[:,subject_2_3]
+        result_aggregate[:,11] = logit_target[:,subject_2_3+1:last_position-1].mean(dim=1)
+        result_aggregate[:,12] = logit_target[:,last_position-1]
+        result_aggregate[:,13] = logit_target[:,last_position]
+        return result_aggregate
 
     def residual_stram_track_target(self,   length, target="copy", disable_tqdm=False, plot=False):
         assert target in ["copy", "mem"], "target should be one of copy or mem"
@@ -384,31 +446,7 @@ class OVCircuit:
             logit_target[layer] = -100 * (logit_target[layer] - avg_mean[layer]) / avg_mean[layer]
         # aggregate for positions
         object_positions = self.dataset.obj_pos[0]
-        subject_1_1 = 5
-        subject_1_2 = 6
-        subject_1_3 = 7 if length > 17 else 6
-        subject_2_1 = object_positions + 2
-        subject_2_2 = object_positions + 3
-        subject_2_3 = object_positions + 4 if length > 17 else object_positions + 3
-        last_position = length - 1
-        object_positions_pre = object_positions - 1
-        object_positions_next = object_positions + 1
-        
-        result_aggregate = torch.zeros((self.model.cfg.n_layers, 14))
-        result_aggregate[:,0] = logit_target[:,:subject_1_1].mean(dim=1)
-        result_aggregate[:,1] = logit_target[:,subject_1_1]
-        result_aggregate[:,2] = logit_target[:,subject_1_2]
-        result_aggregate[:,3] = logit_target[:,subject_1_3]
-        result_aggregate[:,4] = logit_target[:,subject_1_3+1:object_positions_pre].mean(dim=1)
-        result_aggregate[:,5] = logit_target[:,object_positions_pre]
-        result_aggregate[:,6] = logit_target[:,object_positions]
-        result_aggregate[:,7] = logit_target[:,object_positions_next]
-        result_aggregate[:,8] = logit_target[:,subject_2_1]
-        result_aggregate[:,9] = logit_target[:,subject_2_2]
-        result_aggregate[:,10] = logit_target[:,subject_2_3]
-        result_aggregate[:,11] = logit_target[:,subject_2_3+1:last_position-1].mean(dim=1)
-        result_aggregate[:,12] = logit_target[:,last_position-1]
-        result_aggregate[:,13] = logit_target[:,last_position]
+        result_aggregate = self.aggregate_result(object_positions, logit_target, length)
         
         if plot:
             self.plot_heatmap(
@@ -438,3 +476,38 @@ class OVCircuit:
             
         
         return result
+    
+    
+class Investigate_single_head():
+    def __init__(self, model:WrapHookedTransformer, dataset:MyDataset, batch_size):
+        self.model = model
+        self.dataset = dataset
+        self.batch_size = batch_size
+        
+    def get_logit_target_single_head_single_len(self, layer, head, length):
+        self.dataset.set_len(length, self.model)
+        self.dataset.slice_to_fit_batch(self.batch_size)
+        dataloader = torch.utils.data.DataLoader(self.dataset, batch_size=self.batch_size, shuffle=False)
+        num_batches = len(dataloader)
+        
+        logit_mem = torch.zeros((num_batches, self.batch_size))
+        logit_cp = torch.zeros((num_batches, self.batch_size))
+        for idx, batch in enumerate(dataloader):
+            logit, cache = self.model.run_with_cache(batch["corrupted_prompts"])[:,-1,:]
+            mem, cp = to_logit_token(logit, batch["target"])
+            logit_mem[idx] = mem.cpu()
+            logit_cp[idx] = cp.cpu()
+        logit_mem = einops.rearrange(logit_mem, "b s -> (b s)")
+        logit_cp = einops.rearrange(logit_cp, "b s -> (b s)")
+        return logit_mem, logit_cp
+    
+    def get_logit_target_single_head(self, layer, head):
+        lenghts = self.dataset.get_lengths()
+        logit_mem = {}
+        logit_cp = {}
+        for l in lenghts:
+            logit_mem[l], logit_cp[l] = self.get_logit_target_single_head_single_len(layer, head, l)
+        
+        logit_mem = torch.cat(list(logit_mem.values()), dim=0)
+        logit_cp = torch.cat(list(logit_cp.values()), dim=0)
+        return logit_mem, logit_cp
