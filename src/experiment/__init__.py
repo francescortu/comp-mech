d8d0f2041e0de3518868eff058e35338053ad77b
Francesc0rtu <francescortu@live.it>
2023-12-04 19:04:29 +0100

    update

--- src/experiment/__init__.py
+++ src/experiment/__init__.py
@@ -1,3 +1,3 @@
-from src.experiment.ablate import Ablate, AblateMultiLen
+from src.experiment.ablate import Ablate, AblateMultiLen, AblateMLP
 from src.experiment.attention import AttentionPattern
 from src.experiment.ov_residual import OVCircuit
