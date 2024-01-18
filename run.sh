cbf73796998b356ad3dab25d76462644fe7be9e2
Fracesc0rtu <francescortu@live.it>
2024-01-18 08:49:27 +0000

    update run script

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH -n10
-#SBATCH --job-name=train
+#SBATCH --job-name=pythia6b
 #SBATCH -N1
 #SBATCH -p DGX
 #SBATCH --gpus=1
 #SBATCH --mem=200gb
-#SBATCH --time=48:00:00
+#SBATCH --time=200:00:00
 #SBATCH --output=my_job_%j.out
 #SBATCH --error=my_job_%j.err
 
 conda activate torch
 cd script
-python run_all.py --all --no-plot --batch 100
+python run_all.py --ablate --no-plot --model pythia-6.9b --batch 5 --total-effect  --ablate-component mlp_out
+#python score_models.py --all
