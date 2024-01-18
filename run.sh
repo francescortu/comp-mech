ce16941438f05dbefad4f9b8efd7ca20fbf563ed
Francesc0rtu <francescortu@live.it>
2024-01-18 09:54:00 +0100

    update script

--- run.sh
+++ run.sh
@@ -11,5 +11,9 @@
 
 conda activate torch
 cd script
-python run_all.py --ablate --no-plot --model pythia-6.9b --batch 5 --total-effect  --ablate-component mlp_out
+# python run_all.py --ablate --no-plot --model pythia-6.9b --batch 5 --total-effect  --ablate-component mlp_out
+python run_all.py --ablate --no-plot --model pythia-6.9b --batch 5 --total-effet --ablate-component mlp_out --start 0 --slice 2500
+# python run_all.py --ablate --no-plot --model pythia-6.9b --batch 5 --total-effet --ablate-component mlp_out --start 2500 --slice 5000
+# python run_all.py --ablate --no-plot --model pythia-6.9b --batch 5 --total-effet --ablate-component mlp_out --start 5000 --slice 7500
+# python run_all.py --ablate --no-plot --model pythia-6.9b --batch 5 --total-effet --ablate-component mlp_out --start 7500 --slice 10000
 #python score_models.py --all
