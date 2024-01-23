9f671fdb0a9937f921e7b1c27a2e2434f28d1017
Fracesc0rtu <francescortu@live.it>
2024-01-23 10:52:44 +0100

    orfeo update

--- script/count_group.py
+++ script/count_group.py
@@ -11,8 +11,8 @@ sys.path.append(os.path.join(script_dir, "..", "src"))
 
 
 import json
-model_name = ["gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl", "EleutherAI/pythia-6.9b"]
-similarity_type = ["logit", "word2vec"]
+model_name = ["gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl", "pythia-6.9b"]
+similarity_type = ["word2vec", "logit"]
 
 
 for similarity in similarity_type:
@@ -20,18 +20,24 @@ for similarity in similarity_type:
     for model in model_name:
         print("Model", model)
         data = json.load(open(f"../data/full_data_sampled_{model}_similarity_{similarity}_10000.json", "r"))
-        sim_1, sim_2, sim_3, sim_4 = 0, 0, 0, 0
+        sim_1, sim_2, sim_3, sim_4, sim_true = 0, 0, 0, 0, 0
         position_1 = []
         for i,d in enumerate(data):
-            if d["target_new"] in d["similar_tokens_1"]:
+            if d["target_new"][1:] in d["similar_tokens_1"] or d["target_new"] in d["similar_tokens_1"]:
                 position_1.append(i)
                 sim_1 += 1
-            if d["target_new"] in d["similar_tokens_2"]:
+            if d["target_true"][1:] not in d["similar_tokens_1"]:
+                if d["target_true"][1:] not in d["similar_tokens_2"]:
+                    if d["target_true"][1:] not in d["similar_tokens_3"]:
+                        if d["target_true"][1:] not in d["similar_tokens_4"]:
+                            sim_true +=1
+                            print(d["target_true"])
+            if d["target_new"][1:] in d["similar_tokens_2"] or d["target_new"] in d["similar_tokens_2"]:
                 sim_2 += 1
-            if d["target_new"] in d["similar_tokens_3"]:
+            if d["target_new"][1:] in d["similar_tokens_3"] or d["target_new"] in d["similar_tokens_3"]:
                 sim_3 += 1
-            if d["target_new"] in d["similar_tokens_4"]:
+            if d["target_new"][1:] in d["similar_tokens_4"] or d["target_new"] in d["similar_tokens_4"]:
                 sim_4 += 1
 
-        print(sim_1, sim_2, sim_3, sim_4)
+        print(sim_1, sim_2, sim_3, sim_4, "total find", sim_1+sim_2+sim_3+sim_4, sim_true)
 #compute the mean of position_ 1
