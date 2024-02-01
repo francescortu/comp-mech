6bcb8fe2ac8bff363a569715c56af3a73479969d
Francesc0rtu <francescortu@live.it>
2024-02-01 17:10:39 +0100

    update

--- src/dataset.py
+++ src/dataset.py
@@ -344,13 +344,14 @@ class BaseDataset(Dataset):
         num_of_samples = len(similarity_score_list)
         num_of_group = num_of_samples // 1000
         group_intervals = [similarity_score_list[(i) * 1000] for i in range(num_of_group)]
+        print("DEBUG: group interval", group_intervals)
         for d in self.full_data:
             similarity_score = d["similarity_score"]
             if similarity_score == -100:
                 d["similarity_group"] = -100
                 continue
             for i in range(num_of_group, -1, -1):
-                if similarity_score >= similarity_score_list[group_intervals[i]]:
+                if similarity_score >= group_intervals[i]:
                     d["similarity_group"] = i
                     break
         return self.full_data
